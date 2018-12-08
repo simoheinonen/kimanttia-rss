3b4b3248bfaf4052bc4fd9a1d0c9fa719857bc60
Simo Heinonen <simo@dealdash.com>
2018-12-08 22:29:15 +0200

    Init

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Suin\RSSWriter\Channel;
+use Suin\RSSWriter\Feed;
+use Suin\RSSWriter\Item;
+use Symfony\Component\DomCrawler\Crawler;
+
+$wwq = file_get_contents('https://radioplay.fi/podcast/kimanttia/');
+
+$crawler = new Crawler($wwq);
+
+$crawler = $crawler->filter('#play_history_list > ul > li > div.recently-played-track > a');
+
+
+$feed = new Feed();
+
+$channel = new Channel();
+$channel
+    ->title('Kimanttia')
+    ->appendTo($feed);
+
+
+foreach ($crawler as $domElement) {
+    $title = $domElement->textContent;
+    $url = 'https://radioplay.fi/' . $domElement->getAttribute('href');
+
+    $item = new Item();
+    $item
+        ->title($title)
+        ->url($url)
+        ->appendTo($channel);
+}
+
+echo $feed->render();
